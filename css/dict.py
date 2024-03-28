c067ae1d3d1f7c859533198027ae662fc933177e
variyajatish <jatishvariya2003@gmail.com>
2024-03-28 13:56:30 +0530

    wi

--- css/dict.py
+++
@@ -1,6 +0,0 @@
-num=int(input("Enter he value In num:"))
-for i in range(1,num):
-    a=str(input("Enter Name:"))
-    b=int(input("Enter Integer Number:"))
-    f={"Name":a,"Int":b}
-print(f)
--- css/mod.py
+++
@@ -1,5 +0,0 @@
-import sys 
-from math import *
-print(pi)
-for i in sys.path:
-    print(i)
